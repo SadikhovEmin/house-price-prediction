c9fad34541aaff2ddee6e74a9ba1d983824dbb66
SadikhovEmin <eemin.sadykhov@gmail.com>
2021-09-03 10:46:32 +0400

    Update

--- house_price_prediction/model.py
+++ house_price_prediction/model.py
@@ -33,16 +33,16 @@ if __name__ == '__main__':
     print(housing_price.columns)
 
     ''' Analyze the data '''
-    # sns.pairplot(housing_price)
-    #
-    # housing_price.hvplot.hist(by='Price', subplots=False, width=1000)
-    # housing_price.hvplot.hist("Price")
-    # housing_price.hvplot.scatter(x='Avg. Area House Age', y='Price')
-    # housing_price.hvplot.scatter(x='Avg. Area Income', y='Price')
-    # housing_price.columns
-    # sns.heatmap(housing_price.corr(), annot=True)
-    #
-    # plt.show()
+    sns.pairplot(housing_price)
+
+    housing_price.hvplot.hist(by='Price', subplots=False, width=1000)
+    housing_price.hvplot.hist("Price")
+    housing_price.hvplot.scatter(x='Avg. Area House Age', y='Price')
+    housing_price.hvplot.scatter(x='Avg. Area Income', y='Price')
+    housing_price.columns
+    sns.heatmap(housing_price.corr(), annot=True)
+
+    plt.show()
 
     ''' Train test split '''
     X = housing_price[['Avg. Area Income', 'Avg. Area House Age', 'Avg. Area Number of Rooms',
